7dcb621de6b7f81e6c691a695b2f30abf0fd280e
Wouter <Wouter@Lapto-Wouter>
2014-04-29 13:19:56 +0200

    tesjesss

---
+++ src/backend/testjessss.java
@@ -0,0 +1,12 @@
+package src.backend;
+
+public class testjessss {
+	
+	public static void main(String args[]){
+		System.out.println("testejsss whoehoe");
+		
+		Connector nice = new Connector();
+		nice.Connect();
+	}
+
+}
