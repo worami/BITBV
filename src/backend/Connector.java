bb0bdc136ff0c69b9dc4bc73b820f8e6bc064885
Wouter <Wouter@Lapto-Wouter>
2014-04-29 12:50:42 +0200

    update

--- src/backend/Connector.java
+++ src/backend/Connector.java
@@ -1,5 +1,55 @@
 package backend;
 
-public class Connector {
+package backend;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Connector {
+	Connection con = null;
+    Statement st = null;
+    ResultSet rs = null;
+
+    String url = "jdbc:mysql://localhost:3306/testdb";
+    String user = "test";
+    String password = "test";
+
+    public void Connect(){
+    	try {
+            con = DriverManager.getConnection(url, user, password);
+            st = con.createStatement();
+            rs = st.executeQuery("SELECT VERSION()");
+
+            if (rs.next()) {
+                System.out.println(rs.getString(1));
+            }
+
+        } catch (SQLException ex) {
+            System.out.println("error eerste");
+            
+        } finally {
+            Close();
+        }
+    }
+    
+    public void Close(){
+    	try {
+            if (rs != null) {
+                rs.close();
+            }
+            if (st != null) {
+                st.close();
+            }
+            if (con != null) {
+                con.close();
+            }
+
+        } catch (SQLException ex) {
+        	System.out.println("Error tweede");
+        }
+    }
 }
+
