36f07f4082eeafc1bc70b109f0db11308988e8be
Wouter <Wouter@Lapto-Wouter>
2014-05-15 14:28:13 +0200

    Updater werkt

--- src/backend/Getter.java
+++ src/backend/Getter.java
@@ -20,25 +20,65 @@ public class Getter {
 	}
 	
 	public void synchronize(){
+		for(CalendarItem c : Splitter.split(http.sendGet())){
+			this.putCalandarItemOwn(c);
+		}
 		
 		for(CalendarItem c : this.getCalendarListInsight()){
-			if(this.getCalendarListOwn().contains(c)){
+			http.sendPost(c);
+			
+			//if(this.bevat(getCalendarListOwn(), hashkey)){
 				
-			} else {
-				http.sendPost(c);
-			}
+			//} else {
+				
+			//}
 			
 			System.out.println(c.toString());
 			//http.sendPost(c);
 		}
 	}
 	
+	public static boolean bevat(List<CalendarItem> lijst, int hashkey){
+		boolean result = false;
+		for(CalendarItem c : lijst){
+			if(Hasher.hash(c.getBookingnr(), c.getContainernr()) == hashkey){
+				result = true;
+			}
+		}
+		return result;
+	}
+	
 	private List<CalendarItem> getCalendarListInsight(){
     	List<CalendarItem> result = new ArrayList<CalendarItem>();
     	ResultSet rs = insight.query("SELECT `Booking`, `Pickup`, `ContainerNumber`, `ImportDocNr`, `NumberColli` FROM `modalitycontainerstatisticsinfocumm` WHERE `Client` = 'TIMBAL' AND `Pickup` > 1398942018000");
+    	
     	try {
 			while(rs.next()){
-				CalendarItem booking = new CalendarItem(rs.getInt(1), rs.getLong(2), rs.getString(3), rs.getString(4), rs.getInt(5));
+				int bookingnr = rs.getInt(1);
+				long start = rs.getLong(2);
+				String containernr = rs.getString(3);
+				String mrn = rs.getString(4);
+				int kartons = rs.getInt(5);
+				
+				CalendarItem booking;
+				
+				ResultSet rsown = own.query("SELECT planned, eta, units, gasmeting, categorie, status, opmerkingen FROM `test` WHERE hashkey = " + Hasher.hash(bookingnr, containernr));
+				
+				if(rsown.next()){
+					start = rsown.getLong(1);
+					long eta = rsown.getLong(2);
+					int units = rsown.getInt(3);
+					boolean gasmeting = rsown.getBoolean(4);
+					char categorie = rsown.getString(5).toCharArray()[0];
+					int status = rsown.getInt(6);
+					String opmerkingen = rsown.getString(7);
+					booking = new CalendarItem(bookingnr, start, containernr, mrn, kartons, units, eta, gasmeting, categorie, status, opmerkingen);
+				} else {
+					booking = new CalendarItem(bookingnr, start, containernr, mrn, kartons);
+				}
+				
+				own.Close();
+				
 				result.add(booking);
 			}
 		} catch (SQLException e) {
@@ -48,16 +88,16 @@ public class Getter {
     	return result;
     }
 	
-	private List<CalendarItem> getCalendarListOwn(){
+	private List<CalendarItem> getCalendarListOwn(int hashkey){
 		List<CalendarItem> result = new ArrayList<CalendarItem>();
-    	ResultSet rs = own.query("SELECT `Booking`, `Pickup`, `ContainerNumber`, `ImportDocNr`, `NumberColli` FROM `modalitycontainerstatisticsinfocumm` WHERE `Client` = 'TIMBAL' AND `Pickup` > 1398942018000");
+    	ResultSet rs = own.query("SELECT hashkey, planned, eta, units, gasmeting, categorie, status, opmerkingen FROM `test` WHERE hashkey = " + hashkey);
     	try {
 			while(rs.next()){
 				CalendarItem booking = new CalendarItem(rs.getInt(1), rs.getLong(2), rs.getString(3), rs.getString(4), rs.getInt(5));
 				result.add(booking);
 			}
 		} catch (SQLException e) {
-			System.out.println("error getter getCaelndatList: " + e.getMessage());
+			System.out.println("error getter getCaelndatListown: " + e.getMessage());
 		}
     	own.Close();
     	return result;
