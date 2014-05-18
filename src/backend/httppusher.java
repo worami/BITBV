6bb231bc37a0985d80ad446d63b1d1c0fea63889
Wouter <Wouter@Lapto-Wouter>
2014-05-18 13:56:22 +0200

    Nu ook delete, update en specifieke get functies

--- src/backend/httppusher.java
+++ src/backend/httppusher.java
@@ -4,12 +4,15 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.Properties;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpDelete;
+import org.apache.http.client.methods.HttpPut;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.entity.StringEntity;
 
@@ -17,20 +20,23 @@ import calendar.CalendarItem;
  
 public class httppusher {
  
-	private final String USER_AGENT = "Mozilla/5.0";
-	
 	final String URL = "http://insight.exomodal.com:80/collectionapi/calendar_items";
 	final String TOKEN = "9900aa";
 	
 	final String DBtemptype = "status";
 	final int DBoperatorid = 23; 
+	
+	private Properties properties;
+	
+	public httppusher(String propertiesFile){
+		//this.properties = p;
+	}
  
 	/**
 	 * Returnt een string met de calendar_items collection
 	 * @return String zoals: [{"_id":"2QCGtYTdxrcQezJsM","operatorid":3}]
 	 */
 	public String sendGet() {
- 
 		HttpClient client = HttpClientBuilder.create().build();
 		HttpGet request = new HttpGet(URL);
 		
@@ -51,10 +57,29 @@ public class httppusher {
  
 		return result;
 	}
+	
+	public CalendarItem sendGet(String id){
+		HttpClient client = HttpClientBuilder.create().build();
+		HttpGet get = new HttpGet(URL + '/' + id);
+		
+		// add request header
+		get.setHeader("X-Auth-Token", TOKEN);
+ 
+		HttpResponse response;
+		CalendarItem result = null;
+		try {
+			response = client.execute(get);
+			BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+			ArrayList<CalendarItem> lijst = Splitter.split(rd.readLine());
+			result = (lijst.size() > 0) ? lijst.get(0) : null;
+		} catch (IOException e) {
+			System.out.println("error: http get");
+		}
+		return result;
+	}
  
 	// HTTP POST request
 	public void sendPost(CalendarItem item){
- 
 		HttpClient client = HttpClientBuilder.create().build();
 		HttpPost post = new HttpPost(URL);
  
@@ -62,36 +87,62 @@ public class httppusher {
 		post.setHeader("X-Auth-Token", TOKEN);
 
 		//String test = "{\"operatorid\":23,\"templatetype\":\"status\",\"typeid\":2,\"id\":7,\"allDay\":false,\"start\":1399970000,\"end\":1399973000,\"title\":\"HOI blala\"}";
-		
 		StringEntity se;
 		try {
 			se = new StringEntity(item.toHTTPString());
-			//se.setContentEncoding("UTF-8");
-		    //se.setContentEncoding(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
 		    post.setEntity(se);
+		    client.execute(post);
 		} catch (UnsupportedEncodingException e) {
-			System.out.println("error: sendPost 1");
-		}
-
-		HttpResponse response;
-		
-		System.out.println(item.toHTTPString());
-		
-		
-		try {
-			response = client.execute(post);
+			System.err.println("error: sendPost 1");
 		} catch (IOException e) {
-			System.out.println("error: sendPost 2");
-		}
+			System.err.println("error: sendPost 2");
+		} 
+	}
+	
+	public void deleteAll(){
+		
+	}
+	
+	public void sendDelete(String id) {
+		HttpClient client = HttpClientBuilder.create().build();
+		HttpDelete del = new HttpDelete(URL + '/' + id);
+		
+		// add request header
+		del.setHeader("X-Auth-Token", TOKEN);
  
+		try {
+			client.execute(del);
+		} catch (IOException e) {
+			System.err.println("error: http del" + e.getMessage());
+		}
+	}
+	
+	public void sendUpdate(String id) {
+		HttpClient client = HttpClientBuilder.create().build();
+		HttpPut put = new HttpPut(URL + '/' + id);
+		
+		// add request header
+		put.setHeader("X-Auth-Token", TOKEN);
+ 
+		StringEntity se;
+		try {
+			se = new StringEntity("{\"$set\":{\"opmerkingen\":\"Geupdate door pusher!\"}}");
+			put.setEntity(se);
+			client.execute(put);
+		} catch (IOException e) {
+			System.err.println("error: http del" + e.getMessage());
+		}
 	}
 	
 	public static void main(String[] args) throws Exception {
 		 
-		httppusher http = new httppusher();
+		httppusher http = new httppusher("database.properties");
  
 		//System.out.println("Testing 1 - Send Http GET request");
-		System.out.println(http.sendGet());
+		//System.out.println(http.sendGet());
+		//http.sendDelete("MLzTkF9EBLjk4br8A");
+		//http.sendUpdate("gZ6WjM4TyjfEkGYw7");
+		System.out.println(http.sendGet("gZ6WjM4TyjfEkGYw7").toString());
 		//http.sendGet();
 		//System.out.println("\nTesting 2 - Send Http POST request");
 		//http.sendPost();
