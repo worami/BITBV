7bf9290d34a28b60a9569cf0a055494df0c0963e
Alexander <alex2143@gmail.com>
2014-05-27 17:08:10 +0200

    Hasher heeft nu een reverse functie

--- src/backend/Hasher.java
+++ src/backend/Hasher.java
@@ -10,6 +10,12 @@ public class Hasher {
 		}
 		long time = System.currentTimeMillis() - timeStart;
 		System.out.println("Time taken for " + NRRUNS + " is: " + time + " ms.");
+		
+		Object[] reverse = reverse(hash(214365,"CMDP 123456 7"));
+
+		System.out.println(reverse[0]);
+		System.out.println(reverse[1]);
+		
 	}
 	
 	/**
@@ -28,8 +34,8 @@ public class Hasher {
 		String cnr2 = cleanContainerNr.substring(4);
 		
 		String bin_bookingnr = 	"0000000000000000000"; //19
-		String bin_cnr1 = 	"0000000000000000000"; //19
-		String bin_cnr2 = 	"000000000000000000000000"; //24
+		String bin_cnr1 = 		"0000000000000000000"; //19
+		String bin_cnr2 = 		"000000000000000000000000"; //24
 		
 		bin_bookingnr += Integer.toBinaryString(bookingnr);
 		bin_bookingnr = bin_bookingnr.substring(bin_bookingnr.length()-19); //levert een binary substring van het bookingnr op met leading nullen
@@ -60,6 +66,33 @@ public class Hasher {
 		for (int i = 0; i < 4; i++) {
 			result += (str.charAt(i) - 'A') * Math.pow(26,3-i);
 		}
+		return result;
+	}
+	
+	//levert bookingnr en containernr op.
+	public static final Object[] reverse(long hash) {
+		Object[] result = new Object[2];
+		
+		String binaryString = Long.toBinaryString(hash);
+		String binary_bookingnr = binaryString.substring(0, binaryString.length()-24-19);
+		String binary_cnr1 = binaryString.substring(binaryString.length()-24-19, binaryString.length()-24);
+		String binary_cnr2 = binaryString.substring(binaryString.length()-24, binaryString.length());
+		result[0] = Integer.parseInt(binary_bookingnr, 2);
+		
+		
+		int cnr1 = Integer.parseInt(binary_cnr1, 2);
+		char cnr1_4 = (char) (cnr1%26+'A');
+		cnr1/=26;
+		char cnr1_3 = (char) (cnr1%26+'A');
+		cnr1/=26;
+		char cnr1_2 = (char) (cnr1%26+'A');
+		cnr1/=26;
+		char cnr1_1 = (char) (cnr1%26+'A');
+		
+		String cnr2_temp = Long.parseLong(binary_cnr2,2)+"";
+		String cnr2 = cnr2_temp.substring(0,6)+" "+ cnr2_temp.substring(6,7);
+		
+		result[1] = ""+cnr1_1 + cnr1_2 + cnr1_3 + cnr1_4 + " " + cnr2;
 		return result;
 	}
 }
